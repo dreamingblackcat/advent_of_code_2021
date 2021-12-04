5c4e7fd889363e6ae564287e4d735bb3a2b00aa2
dreamingblackcat <dreamingblackcat10@gmail.com>
2021-12-04 20:01:38 +0900

    Day 1 and initial commit

---
+++ day_01/sonar_sweep_solution.rb
@@ -0,0 +1,31 @@
+readings = File.read('2021_01_sonar_sweep.txt').split("\n")
+
+def solve_puzzle1(readings)
+  last_reading = readings.first
+  count = 0
+
+  readings[1..].each do |reading|
+    count += 1 if last_reading.to_i < reading.to_i
+    last_reading = reading
+  end
+
+  puts count
+end
+
+# solve_puzzle1(readings)
+
+def solve_puzzle2(readings)
+  a, b, c = readings[0..2]
+  count = 0
+
+  readings[3..].each do |reading|
+    # puts "OOps #{last_reading}:#{reading}" if last_reading.length < reading.length
+    count += 1 if a.to_i + b.to_i + c.to_i < b.to_i + c.to_i + reading.to_i
+    a = b
+    b = c
+    c = reading
+  end
+
+  puts count
+end
+solve_puzzle2(readings)
