74db53268d95ee5e377ded453dc6a1cfd3289d46
dreamingblackcat <dreamingblackcat10@gmail.com>
2021-12-07 23:16:44 +0900

    Day 05: problem 1 solution

---
+++ day_05/hydro_thermal_vents_solution.rb
@@ -0,0 +1,101 @@
+Point = Struct.new(:x, :y) do
+  def <=>(point2)
+    x_comparison = x <=> point2.x
+
+    if x_comparison.zero?
+      y <=> point2.y
+    else
+      x_comparison
+    end
+  end
+end
+
+class PointRange
+  def initialize(point1, point2)
+    @point1, @point2 = normalize_points([point1, point2])
+  end
+
+  def points
+    if vertical_line?
+      ((@point1.y)..(@point2.y)).map do |y|
+        Point.new(@point1.x, y)
+      end
+    elsif horizontal_line?
+      ((@point1.x)..(@point2.x)).map do |x|
+        Point.new(x, @point1.y)
+      end
+    else
+      []
+    end
+  end
+
+  private
+
+  def vertical_line?
+    @point1.x == @point2.x
+  end
+
+  def horizontal_line?
+    @point1.y == @point2.y
+  end
+
+  def unsupported_line_segment?
+    !vertical_line? && !horizontal_line?
+  end
+
+  def normalize_points(points)
+    points.sort
+  end
+end
+
+
+class ThermalVentDetector
+  def initialize
+    @hash = {}
+  end
+
+  def add_thermal_vent(point_range)
+    point_range.points.each do |point|
+      current_count = @hash.fetch(point, 0)
+      @hash[point] = current_count + 1
+    end
+  end
+
+  def marked_points
+    @hash.keys
+  end
+
+  def dangerous_points
+    @hash.select {|point, count| count > 1 }.keys
+  end
+end
+
+class ThermalVentReport
+  attr_reader :point_ranges
+
+  def initialize(file_name)
+    @report = File.readlines(file_name, chomp: true)
+    @thermal_vent_detector = ThermalVentDetector.new
+    @point_ranges = @report.map do |point_range_string|
+      # point_range_string is of the format '0,5 -> 0,9'
+      point_a, point_b = point_range_string.split(' -> ')
+      point_range = PointRange.new(
+        Point.new(*point_a.split(',').map(&:to_i)),
+        Point.new(*point_b.split(',').map(&:to_i)),
+      )
+      @thermal_vent_detector.add_thermal_vent(point_range)
+    end
+  end
+
+  def dangerous_points
+    @thermal_vent_detector.dangerous_points
+  end
+end
+
+
+
+if __FILE__ == $PROGRAM_NAME
+  report = ThermalVentReport.new('./hyrdro_thermal_vents_input.txt')
+
+  puts "Hydro Thermal Vent Report: dangerous points count is #{report.dangerous_points.length}"
+end
---
+++ day_05/hyrdo_thermal_vents_test.rb
@@ -0,0 +1,109 @@
+require 'minitest/autorun'
+require_relative './hydro_thermal_vents_solution.rb'
+
+describe PointRange do
+  it 'enumerates points on a given range (horizontal)' do
+    point_range = PointRange.new(Point.new(5, 9), Point.new(0, 9))
+
+    expected_points = [
+      Point.new(0, 9),
+      Point.new(1, 9),
+      Point.new(2, 9),
+      Point.new(3, 9),
+      Point.new(4, 9),
+      Point.new(5, 9),
+    ]
+
+    assert_equal(expected_points, point_range.points,
+      "Should give correct points")
+  end
+
+  it 'enumerates points on a given range (vertical)' do
+    point_range = PointRange.new(Point.new(7, 0), Point.new(7, 4))
+
+    expected_points = [
+      Point.new(7, 0),
+      Point.new(7, 1),
+      Point.new(7, 2),
+      Point.new(7, 3),
+      Point.new(7, 4),
+    ]
+
+    assert_equal(expected_points, point_range.points,
+      "Should give correct points")
+  end
+end
+
+
+describe 'ThermalVentDetector' do
+  it 'can mark a point range.' do
+    horizonal_line_vent = PointRange.new(Point.new(0,9), Point.new(5,9))
+    detector = ThermalVentDetector.new
+    detector.add_thermal_vent(horizonal_line_vent)
+
+    assert_equal(6, detector.marked_points.length,
+      "Should mark all 6 points")
+  end
+
+  it 'can differentiate overlapping points.' do
+    horizonal_line_vent = PointRange.new(Point.new(0,9), Point.new(5,9))
+    vertical_line_vent  = PointRange.new(Point.new(0,7), Point.new(0,9))
+    detector = ThermalVentDetector.new
+    detector.add_thermal_vent(horizonal_line_vent)
+    detector.add_thermal_vent(vertical_line_vent)
+
+    assert_equal(8, detector.marked_points.length,
+      "Should only have 8 marked points neglecting an overlap.")
+  end
+
+  it 'can determine total number of dangerous points marked so far.' do
+    horizonal_line_vent = PointRange.new(Point.new(0,9), Point.new(5,9))
+    vertical_line_vent  = PointRange.new(Point.new(0,7), Point.new(0,9))
+    another_overlapping_vent  = PointRange.new(Point.new(0,5), Point.new(0,9))
+
+    detector = ThermalVentDetector.new
+    detector.add_thermal_vent(horizonal_line_vent)
+    detector.add_thermal_vent(vertical_line_vent)
+    detector.add_thermal_vent(vertical_line_vent)
+
+    assert_equal(3, detector.dangerous_points.length,
+      "Should only have 3 dangerours points.")
+  end
+
+  it 'ignores point ranges that are neither horizontal nor vertical.' do
+    horizonal_line_vent = PointRange.new(Point.new(0,9), Point.new(5,9))
+    vertical_line_vent  = PointRange.new(Point.new(0,7), Point.new(0,9))
+    non_horizontal_nor_vertical  = PointRange.new(Point.new(0,5), Point.new(4,9))
+
+    detector = ThermalVentDetector.new
+    detector.add_thermal_vent(horizonal_line_vent)
+    detector.add_thermal_vent(vertical_line_vent)
+
+    marked_points_so_far = detector.marked_points
+
+    detector.add_thermal_vent(non_horizontal_nor_vertical)
+
+    assert_equal(marked_points_so_far, detector.marked_points,
+      "Should still have same old marked_points without adding new ones.")
+  end
+end
+
+describe 'ThermalVentReport' do
+  before do
+    @input = File.readlines('./sample_test_input.txt', chomp: true)
+  end
+
+  it 'parses a given thermal vent report file correctly.' do
+    report = ThermalVentReport.new('sample_test_input.txt')
+
+    assert_equal(10, report.point_ranges.length,
+      "Should have parsed 10 point ranges.")
+  end
+
+  it 'can find dangerous points' do
+    report = ThermalVentReport.new('sample_test_input.txt')
+
+    assert_equal(5, report.dangerous_points.length,
+      "Should have detected 5 dangerous points.")
+  end
+end
